2019a8ca07bcaeebb39fc876fff65ce9a56322d2
Chris Tander <stxmple@gmail.com>
2018-03-05 15:53:53 +1100

    Player anims and sprites

---
+++ Assets/~SunnyLand/Custom_Scripts/CameraTarget.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraTarget : MonoBehaviour {
+    public Transform Target;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        
+	}
+
+    void LateUpdate()
+    {
+        gameObject.GetComponent<Transform>().position = Vector3.Lerp(gameObject.GetComponent<Transform>().position, Target.position, Time.deltaTime * 10);
+    }
+}
